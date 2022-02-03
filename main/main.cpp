c1ec78bf6b6f65027f01cfa613295a72f426d488
sukrisl <sukriansyah.laksono@efishery.com>
2022-02-03 17:51:59 +0700

    Test out char to binary function

--- main/main.cpp
+++ main/main.cpp
@@ -1,40 +1,45 @@
-/* Hello World Example
-
-   This example code is in the Public Domain (or CC0 licensed, at your option.)
-
-   Unless required by applicable law or agreed to in writing, this
-   software is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
-   CONDITIONS OF ANY KIND, either express or implied.
-*/
-#include <stdio.h>
+#include <cstdio>
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
-#include "esp_system.h"
-#include "esp_spi_flash.h"
 
-
-void app_main(void)
-{
-    printf("Hello world!\n");
-
-    /* Print chip information */
-    esp_chip_info_t chip_info;
-    esp_chip_info(&chip_info);
-    printf("This is ESP32 chip with %d CPU cores, WiFi%s%s, ",
-            chip_info.cores,
-            (chip_info.features & CHIP_FEATURE_BT) ? "/BT" : "",
-            (chip_info.features & CHIP_FEATURE_BLE) ? "/BLE" : "");
-
-    printf("silicon revision %d, ", chip_info.revision);
-
-    printf("%dMB %s flash\n", spi_flash_get_chip_size() / (1024 * 1024),
-            (chip_info.features & CHIP_FEATURE_EMB_FLASH) ? "embedded" : "external");
-
-    for (int i = 10; i >= 0; i--) {
-        printf("Restarting in %d seconds...\n", i);
-        vTaskDelay(1000 / portTICK_PERIOD_MS);
+void charToBinary(bool(&dest)[8], char c) {
+    for (uint8_t i = 0; i < 8; i++) {
+        dest[i] = static_cast<uint8_t>(c) & (0b10000000 >> i);
     }
-    printf("Restarting now.\n");
-    fflush(stdout);
-    esp_restart();
+
+    printf("%c,0x%02x,%u%u%u%u%u%u%u%u\n",
+        c, static_cast<uint8_t>(c),
+        dest[0], dest[1], dest[2], dest[3], dest[4], dest[5], dest[6], dest[7]
+    );
+}
+
+void strReadTask(void* pvParam) {
+    for(;;) {
+        const char* inputStr = 
+R"(
+One of these days i'm going to get that red dot, just you wait and see get my claw stuck in the dog's ear.
+Crash against wall but walk away like nothing happened purr when being pet, so stick butt in face scratch
+at door to be let outside, get let out then scratch at door immmediately after to be let back in kitty pounce,
+trip, faceplant you didn't see that no you didn't definitely didn't lick, lick, lick, and preen away the embarrassment
+but caticus cuteicus. Flex claws on the human's belly and purr like a lawnmower. Present belly, scratch hand when stroked.
+Scratch bawl under human beds.
+)";
+
+        printf("\nWait a sec...\n\n");
+        vTaskDelay(1000 / portTICK_PERIOD_MS);
+
+        int64_t tlog_us = esp_timer_get_time();
+
+        for (uint64_t i = 0; inputStr[i] != '\0'; i++) {
+            bool binaryResult[8];
+            charToBinary(binaryResult, inputStr[i]);
+            vTaskDelay(500 / portTICK_PERIOD_MS);
+        }
+
+        printf("Execution time: %llu ms\n", (esp_timer_get_time() - tlog_us) / 1000);
+    }
+}
+
+extern "C" void app_main() {
+    xTaskCreate(strReadTask, "String read task", 1024 * 2, NULL, 5, NULL);
 }
